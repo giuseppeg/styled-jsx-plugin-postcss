b6f64e803c14e7b5c5c8be99de7d90d101308953
Giuseppe Gurgone <giuseppeg@users.noreply.github.com>
2021-01-21 20:08:05 +0100

    fixes

--- compile.js
+++ compile.js
@@ -1,85 +1,85 @@
-const { spawnSync } = require("child_process");
-const path = require("path");
+const { spawnSync } = require('child_process')
+const path = require('path')
 const {
   Worker,
   receiveMessageOnPort,
-  MessageChannel,
-} = require("worker_threads");
+  MessageChannel
+} = require('worker_threads')
 
-let worker = null;
-let unreftimeout = null;
+let worker = null
+let unreftimeout = null
 
 function compileWorker(...args) {
   if (unreftimeout) {
-    clearTimeout(unreftimeout);
-    unreftimeout = null;
+    clearTimeout(unreftimeout)
+    unreftimeout = null
   }
 
   if (!worker) {
-    worker = new Worker("./worker.js");
+    worker = new Worker(require.resolve('./worker.js'))
   }
 
-  const signal = new Int32Array(new SharedArrayBuffer(4));
-  signal[0] = 0;
+  const signal = new Int32Array(new SharedArrayBuffer(4))
+  signal[0] = 0
 
   try {
-    const subChannel = new MessageChannel();
+    const subChannel = new MessageChannel()
     worker.postMessage({ signal, port: subChannel.port1, args }, [
-      subChannel.port1,
-    ]);
-    Atomics.wait(signal, 0, 0);
-    const { message } = receiveMessageOnPort(subChannel.port2);
+      subChannel.port1
+    ])
+    Atomics.wait(signal, 0, 0)
+    const { message } = receiveMessageOnPort(subChannel.port2)
     if (message.error) {
-      throw new Error(`postcss failed with ${message.error}`);
+      throw new Error(`postcss failed with ${message.error}`)
     }
-    return message.result;
+    return message.result
   } catch (error) {
-    throw error;
+    throw error
   } finally {
     unreftimeout = setTimeout(() => {
-      worker.unref();
-      worker = null;
-      unreftimeout = null;
-    }, 1000);
+      worker.unref()
+      worker = null
+      unreftimeout = null
+    }, 1000)
   }
 }
 
 function compileSubprocess(css, settings) {
-  const result = spawnSync("node", [path.resolve(__dirname, "subprocess.js")], {
+  const result = spawnSync('node', [path.resolve(__dirname, 'subprocess.js')], {
     input: JSON.stringify({
       css,
-      settings,
+      settings
     }),
-    encoding: "utf8",
-  });
+    encoding: 'utf8'
+  })
 
   if (result.status !== 0) {
-    if (result.stderr.includes("Invalid PostCSS Plugin")) {
-      let isNext = false;
+    if (result.stderr.includes('Invalid PostCSS Plugin')) {
+      let isNext = false
       try {
-        require.resolve("next");
-        isNext = true;
+        require.resolve('next')
+        isNext = true
       } catch (err) {}
       if (isNext) {
         console.error(
-          "Next.js 9 default postcss support uses a non standard postcss config schema https://err.sh/next.js/postcss-shape, you must use the interoperable object-based format instead https://nextjs.org/docs/advanced-features/customizing-postcss-config"
-        );
+          'Next.js 9 default postcss support uses a non standard postcss config schema https://err.sh/next.js/postcss-shape, you must use the interoperable object-based format instead https://nextjs.org/docs/advanced-features/customizing-postcss-config'
+        )
       }
     }
 
-    throw new Error(`postcss failed with ${result.stderr}`);
+    throw new Error(`postcss failed with ${result.stderr}`)
   }
 
-  return result.stdout;
+  return result.stdout
 }
 
 module.exports = (css, settings) => {
   if (
-    typeof receiveMessageOnPort === "undefined" ||
-    settings.compileEnv === "process"
+    typeof receiveMessageOnPort === 'undefined' ||
+    settings.compileEnv === 'process'
   ) {
-    return compileSubprocess(css, settings);
+    return compileSubprocess(css, settings)
   } else {
-    return compileWorker(css, settings);
+    return compileWorker(css, settings)
   }
-};
+}
--- processor.js
+++ processor.js
@@ -1,20 +1,20 @@
-const postcss = require("postcss");
-const loader = require("postcss-load-config");
+const postcss = require('postcss')
+const loader = require('postcss-load-config')
 
-const loaderPromises = new Map();
+const loaderPromises = {}
 
 module.exports = function processor(src, options) {
-  options = options || {};
+  options = options || {}
 
-  const loaderPromise = loaderPromises.has(options)
-    ? loaderPromises.get(options)
+  const loaderPromise = loaderPromises.hasOwnProperty(options.path || 'auto')
+    ? loaderPromises[options.path || 'auto']
     : loader(options.env || process.env, options.path, {
-        argv: false,
-      }).then((pluginsInfo) => pluginsInfo.plugins || []);
+        argv: false
+      }).then((pluginsInfo) => pluginsInfo.plugins || [])
 
-  loaderPromises.set(options, loaderPromise);
+  loaderPromises[options.path || 'auto'] = loaderPromise
 
   return loaderPromise
     .then((plugins) => postcss(plugins).process(src, { from: false }))
-    .then((result) => result.css);
-};
+    .then((result) => result.css)
+}
